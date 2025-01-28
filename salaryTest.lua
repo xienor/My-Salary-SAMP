971bd7242cb942093292f170cbe3c707a8b24b28
xienor <63422780+xienor@users.noreply.github.com>
2025-01-28 16:07:32 +0300

    Добавлена поддержка кириллицы

--- salaryTest.lua
+++ salaryTest.lua
@@ -1,4 +1,4 @@
-script_name("My Salary")
+п»їscript_name("My Salary")
 script_authors("mihaha")
 script_version("0.1")
 
@@ -7,18 +7,21 @@ local imgui = require 'imgui'
 local events = require 'lib.samp.events'
 local encoding = require 'encoding'
 encoding.default = 'CP1251'
+u8 = encoding.UTF8
 
 local playerMoney = 0
 local currentMoney = 0
 local earned = 0
+local spended = 0
+local sessionSalary = 0
 local main_window_state = imgui.ImBool(false)
 
 function main()
-  print("Loading: My Salary")
+  --print("Loading: My Salary")
   while not isSampAvailable() do wait(0) end
   sampRegisterChatCommand("msalary", openWindow)
   wait(0)
-  print("Command registered")
+  --print("Command registered")
   while true do
 			if sampIsLocalPlayerSpawned() then
         currentMoney = getPlayerMoney(Player)
@@ -27,11 +30,12 @@ function main()
           playerMoney = getPlayerMoney(Player)
           if currentMoney < playerMoney then
             earned = earned + (playerMoney - currentMoney)
-            print("Session salary: " .. earned)
+            --print("Session salary: " .. earned)
           elseif currentMoney > playerMoney then
-            earned = earned - (currentMoney - playerMoney)
-            print("Session salary: " .. earned)
+            spended = spended - (currentMoney - playerMoney)
+            --print("Session salary: " .. earned)
           end
+          sessionSalary = earned + spended
           currentMoney = playerMoney
           wait(0)
         end
@@ -41,10 +45,12 @@ function main()
   end
 function imgui.OnDrawFrame()
     if main_window_state.v then
-      imgui.SetNextWindowSize(imgui.ImVec2(150, 200), imgui.Cond.FirstUseEver)
-      imgui.Begin('My Salary', windowOpen)
-      imgui.Text('Hello world')
-      imgui.Text('Earn: $' .. earned)
+      imgui.ShowCursor = false
+      --imgui.SetNextWindowSize(imgui.ImVec2(150, 200), imgui.Cond.FirstUseEver)
+      imgui.Begin('My Salary', windowOpen, imgui.WindowFlags_AlwaysAutoResize)
+      imgui.Text(u8'Получено: $' .. earned)
+      imgui.Text(u8'Потрачено: $' .. spended)
+      imgui.Text(u8'Итого: $' .. sessionSalary)
       imgui.End()
     end
 end
